4eae5126d5acefd68c61ba8d4db015de76eb2af7
Elizabeth Saltykova <>
2020-11-26 01:11:52 +0300

    Initial commit

---
+++ movor/movor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movor
+//
+//  Created by Elizabeth Saltykova on 26.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
